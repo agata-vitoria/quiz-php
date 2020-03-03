2a1c657013c26e3922d8b107dbf8d147965133f9
AGATA VITORIA DE SOUZA LIMA <469839673@alunos.etec.itaqua>
2020-03-02 22:27:14 -0300

    first commit

---
+++ view/quiz.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perguntas</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <form action="" method="POST">
+        <h4>1 - No filme relíquias da morte parte 2 quem mata Nagini? </h4> 
+        <!-- Neville Longbottom -->
+        <select name="pergunta1">
+            <option value="sv">...</option> 
+            <option value="nl">Neville Longbottom</option> 
+            <option value="hp" selected>Harry Potter</option>
+            <option value="hm">Hermione</option>
+        </select>
+        <!-- <input type="text" name="pergunta1"> -->
+        <h4>2 - Quais são os integrantes do grupo trio de ouro? </h4>
+        <!-- Harry, Hermione e Rony -->
+        <input type="text" name="pergunta2">
+        <h4>3 - Qual é a plataforma para ir a Hogwarts? </h4>
+        <!-- 9/3 4 -->
+        <input type="text" name="pergunta3">
+        <h4>4 - Em que filme Harry, Hermione e Rony descobrem um tal de Nicolau Flamel? </h4>
+        <!-- Pedra Filosofal -->
+        <input type="text" name="pergunta4">
+        <h4>5 - Quantas Horcrux existiam? </h4>
+        <!-- 7 -->
+        <input type="text" name="pergunta5">
+        <h4>6 - Em que filme Luna aparece? </h4>
+        <!-- Ordem da Fênix -->
+        <input type="text" name="pergunta6">
+
+        <p><input type="submit" value="Verificar" class="verificar"/></p>
+    </form>
+</body>
+</html>
